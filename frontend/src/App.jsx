137a610d6a247a2daebd8e85346d805ddd0a2e62
Izmal Muhamad <muhammadizmalmuhamad@gmail.com>
2026-06-23 14:39:35 +0800

    feat: add HomePage navbar
    Add navigation bar to HomePage with functional button to go to CreatePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-
 import HomePage from "./pages/HomePage.jsx";
 import NoteDetailPage from "./pages/NoteDetailPage.jsx";
 import CreatePage from "./pages/CreatePage.jsx";
@@ -6,12 +5,11 @@ import { Route, Routes } from "react-router";
 
 const App = () => {
   return (
-    <div data-theme="night">
-      <button className="btn btn-outline">Click me</button>
+    <div data-theme="forest">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/notes/:id" element={<NoteDetailPage />} />
-        <Route path="/create" element={<CreatePage />} /> 
+        <Route path="/create" element={<CreatePage />} />
       </Routes>
     </div>
   );
